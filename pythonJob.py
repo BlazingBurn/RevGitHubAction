19d375b2e94265ef8fccfefbb030347927395c8f
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2024-03-21 11:15:39 +0100

    build/push docker

--- pythonJob.py
+++ pythonJob.py
@@ -1,13 +1,2 @@
-# a = 2
-# print("coucou", a)
-
-from mon_module import calculer_moyenne
-
-def main():
-    # Exemple d'utilisation de la fonction calculer_moyenne
-    liste = [1, 2, 3, 4, 5]
-    moyenne = calculer_moyenne(liste)
-    print("La moyenne de la liste est:", moyenne)
-
-if __name__ == "__main__":
-    main()
+a = 2
+print("coucou", a)
