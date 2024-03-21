ce20349a71055896141584a4729586269cd53cb9
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2024-03-21 10:30:26 +0100

    update

--- tests/test_calcul_moyenne.py
+++ tests/test_calcul_moyenne.py
@@ -1,4 +1,5 @@
-from mon_module import calculer_moyenne
+from .. import mon_module
+# from mon_module import calculer_moyenne
 
 def test_calcul_moyenne():
     assert calculer_moyenne([1, 2, 3, 4, 5]) == 3
