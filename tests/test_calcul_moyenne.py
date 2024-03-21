9c1d1dfd34923f6cf03b0b606cf90c91bf8648ae
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2024-03-21 09:53:42 +0100

    update

---
+++ tests/test_calcul_moyenne.py
@@ -0,0 +1,7 @@
+from mon_module import calculer_moyenne
+
+def test_calcul_moyenne():
+    assert calculer_moyenne([1, 2, 3, 4, 5]) == 3
+    assert calculer_moyenne([10, 20, 30, 40, 50]) == 30
+    assert calculer_moyenne([]) == 0  # Cas où la liste est vide
+    assert calculer_moyenne([5]) == 5  # Cas où il y a une seule valeur dans la liste
