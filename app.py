2ca3d2552d00959750facb22e63765dee60bdca1
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2024-03-21 12:10:41 +0100

    build/push docker

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Flask!'
+    ip = request.host.split(':')[0]
+    return f'Hello, Flask! Server IP: {ip}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
